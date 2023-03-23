30163907eec47fb9330d4fac1b613a4d2e2b0af9
zeredogo <zeredogo@gmail.com>
2023-03-23 15:51:58 +1400

    update made

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { SearchIcon } from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <Paper 
+      component='form'
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        p1: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 }
+      }}
+     >
+       TEST
+    </Paper>
   )
 }
 
