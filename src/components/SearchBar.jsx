b6a48005d92422786dcdde10c587da25dec32bb7
zeredogo <zeredogo@gmail.com>
2023-03-23 15:10:25 +1400

    update made

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
