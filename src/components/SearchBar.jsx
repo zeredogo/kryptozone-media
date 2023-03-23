881d870969d5a5da035dd364a0bbaaec027c7aa8
zeredogo <zeredogo@gmail.com>
2023-03-23 16:04:20 +1400

    update made

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
-import { SearchIcon } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
@@ -16,7 +16,15 @@ const SearchBar = () => {
         mr: { sm: 5 }
       }}
      >
-       TEST
+       <input 
+         className='search-bar'
+         placeholder='Search...'
+         value=''
+         onChange={() => {}}
+       />
+       <IconButton type='submit' sx={{ p: '1' }}>
+        <Search />
+       </IconButton>
     </Paper>
   )
 }
