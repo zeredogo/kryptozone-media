c88e6202c748f671265280b4992f911e1755588c
zeredogo <zeredogo@gmail.com>
2023-03-29 14:30:48 +1400

    update made

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle,
+demoChannelUrl, demoChannelTitle } from '../utils/constants';
+
+const VideoCard = ({ video: { id:} }) => {
+    
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
