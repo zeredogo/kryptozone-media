0f2ca045d8c8d29ea8db78afaecb059b664de23a
zeredogo <zeredogo@gmail.com>
2023-04-01 05:05:17 -0700

    update made

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
 
-const SearchFeed = () => {
+import { Videos } from './';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+const Feed = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory])
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+       <Typography variant='h4'
+       fontWeight='bold' mb={2} sx={{ color: 'white'}}
+       >
+         {selectedCategory} <span style={{ color: '#F31503' }}>video</span>
+       </Typography>
+       <Videos videos={videos}/>
+  </Box>
   )
 }
 
-export default SearchFeed
+export default Feed
