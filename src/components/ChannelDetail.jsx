43758773bf585b625917ef381667ed59c93f38b9
zeredogo <zeredogo@gmail.com>
2023-03-31 23:17:36 -1200

    update made

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -3,9 +3,20 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 
 const ChannelDetail = () => {
+  const [ChannelDetail, setChannelDetail] = useState(null)
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+  }, [id]) 
   return (
     <div>ChannelDetail</div>
   )
