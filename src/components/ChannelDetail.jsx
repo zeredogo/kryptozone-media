e3375781adcd31e5a76f4ee2f6d1034c6783cda4
zeredogo <zeredogo@gmail.com>
2023-04-01 04:59:37 -0700

    update made

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -7,7 +7,7 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 
 const ChannelDetail = () => {
-  const [ChannelDetail, setChannelDetail] = useState(null)
+  const [channelDetail, setChannelDetail] = useState(null)
   const [videos, setVideos] = useState([]);
   const { id } = useParams();
 
@@ -21,8 +21,20 @@ const ChannelDetail = () => {
 
   return (
     <Box minHeight='95vh'>
-      <Box></Box>
-    </Box>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,364,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }}/>
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{ mr: {sm: '100px'} }}>
+          <Videos videos={videos } />
+        </Box>
+      </Box>
+    </Box> 
   )
 }
 
