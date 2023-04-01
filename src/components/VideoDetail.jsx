719b8c82396e1fde2da17f81f94fd273f9aa4c94
zeredogo <zeredogo@gmail.com>
2023-04-02 06:33:31 +1400

    update made

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,16 +4,20 @@ import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
   fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
   .then((data) => setVideoDetail(data.items[0]));
+
+  fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+  .then((data) => setVideos(data.items));
   }, [id]);
 
   if (!videoDetail?.snippet) return 'Loading...';
@@ -51,6 +55,11 @@ const VideoDetail = () => {
           </Box>
         </Box>
       </Stack>
+
+      <Box px={2} py={{md: 1, xs: 5}} 
+      justifyContent='center' alignItems='center'>
+        <Videos videos={videos} direction='column'/>
+      </Box>
     </Box>
   )
 }
