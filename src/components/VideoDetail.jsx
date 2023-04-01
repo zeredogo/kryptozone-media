831cf2e04f023aa52d06b2a9ba840ea5581294f8
zeredogo <zeredogo@gmail.com>
2023-04-02 03:40:59 +1400

    update made

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,4 +1,4 @@
-import { useState, UseEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
@@ -20,7 +20,11 @@ const VideoDetail = () => {
       <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
-            <ReactPlayer />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+             className='react-player' controls />
+            <Typography color='#fff'>
+              {VideoDetail.snippet.title}
+            </Typography>
           </Box>
         </Box>
       </Stack>
