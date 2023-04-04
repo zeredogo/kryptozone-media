6eece4d7326f6066c74c7e1689f2207c847e1b8a
zeredogo <zeredogo@gmail.com>
2023-04-03 17:13:02 -1000

    update made

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -25,7 +25,7 @@ const ChannelCard = ({ channelDetail, marginTop }) =>  (
           <CardMedia
           image={channelDetail?.snippet?.thumbnails?.high?.url || 
           demoProfilePicture} alt={channelDetail?.snippet?.title}
-          sx={{ borderRadius: '50%', height: '180px', width: '180px', md: 2,
+          sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2,
           border: '1px solid #e3e3e3'}} 
         />
         <Typography variant='h6'>
