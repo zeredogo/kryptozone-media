4a95eafbb3d8919e275491ffe19a50a5e0b7727e
zeredogo <zeredogo@gmail.com>
2023-04-06 10:24:37 +1400

    update made

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -26,7 +26,7 @@ const Feed = () => {
           <Typography className='copyright' variant='body2' 
           sx={{ mt: 1.5, color: '#fff' }}
           >
-            Copyright 2023 Krypto zone  Media
+           Krypto Zone &copy; 2023 
           </Typography>
         </Box>
         <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
