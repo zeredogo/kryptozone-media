6be51699f44488b0b2957a4165ab2994794b0543
zeredogo <zeredogo@gmail.com>
2023-04-04 23:47:14 -1200

    update made

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -32,7 +32,7 @@ const VideoDetail = () => {
           <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
              className='react-player' controls />
-            <Typography color='#fff' variant='h5'>
+            <Typography color='#fff' variant='h5' fontWeight='bold'>
               {title}
             </Typography>
             <Stack direction='row' justifyContent='space-between' sx={{
